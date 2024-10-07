64ed4a972b509576900ab7f5708e2776ed81fef7
joseq <joseq@DESKTOP-SO7GUKG>
2024-10-07 16:29:45 -0600

    Agregar archivos de proyecto.

---
+++ Tienda Arcoiris/Modelo/Ventas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda_Arcoiris.Modelo
+{
+    internal class Ventas
+    {
+    }
+}
