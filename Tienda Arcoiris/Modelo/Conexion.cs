d002a19fe3722311ecf9a36ed2d39d195177a0f2
Jose Quintanilla <josequintanillarodriguez513@gmail.com>
2024-10-07 18:01:31 -0600

    Se agregó la conexión a la base de datos MySQL

--- Tienda Arcoiris/Modelo/Conexion.cs
+++ Tienda Arcoiris/Modelo/Conexion.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,47 @@ namespace Tienda_Arcoiris.Modelo
 {
     internal class Conexion
     {
+        //Metodo para obtener la conexion
+        public MySqlConnection getConexion()
+        {
+
+            MySqlConnection conexion;
+
+            try
+            {
+                // Lee la cadena de conexión desde el archivo app.config
+                string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString;
+
+
+                //Instancia de mysqlconnection utilizando la cadena de conexion
+                conexion = new MySqlConnection(connectionString);
+
+                //Abrir conexion a la base de datos
+                conexion.Open();
+
+                //Devuelve la conexion
+                return conexion;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al establecer la conexion: {ex.Message}");
+                throw;
+            }
+
+
+        }
+
+
+
+        //Metodo para cerrar la conexion
+        public void cerrarConexion(MySqlConnection conexion)
+        {
+            if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+            {
+                //Cierra la conexion si esta abierta
+                conexion.Close();
+            }
+        }
     }
 }
